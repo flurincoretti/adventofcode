15ec17c32030fc6d53fdfb58c8493279aae89213
Flurin Coretti <hello@flurincoretti.com>
2019-12-27 21:44:38 +0100

    Refactor 10 Part 1

--- 10.py
+++ 10.py
@@ -1,31 +1,32 @@
 from math import atan2
 
 
-def get_dimensions(asteroid_map):
+def get_dims(asteroid_map):
     width = asteroid_map.find('\n')
     height = asteroid_map.count('\n') + 1
     return width, height
 
 
-def get_asteroids(asteroid_map):
-    w, h = get_dimensions(asteroid_map)
+def get_coords(asteroid_map):
+    w, h = get_dims(asteroid_map)
     am = asteroid_map.split('\n')
     return [(x,y) for x in range(w) for y in range(h) if am[y][x] == '#']
 
 
-def count_visible_asteroids(asteroids, a):
-    return len(set(atan2(b[1]-a[1], b[0]-a[0]) for b in asteroids if b != a))
+def get_angles(coords, c):
+    return [atan2(b[1]-c[1], b[0]-c[0]) for b in coords if b != c]
 
 
 def part_one(asteroid_map):
-    asteroids = get_asteroids(asteroid_map)
-    counts = [count_visible_asteroids(asteroids, a) for a in asteroids]
-    print("Best is {} with {} other asteroids detected.".format(
-        asteroids[counts.index(max(counts))], max(counts)
-    ))
+    coords = get_coords(asteroid_map)
+    counts = [len(set(get_angles(coords, c))) for c in coords]
+    return coords[counts.index(max(counts))], max(counts)
 
 
 if __name__ == "__main__":
     inputs = open('inputs/10.txt', 'r')
     asteroid_map = inputs.read()
-    part_one(asteroid_map)
+
+    # Part 1
+    best, n = part_one(asteroid_map)
+    print("Best is {} with {} other asteroids detected.".format(best, n))
